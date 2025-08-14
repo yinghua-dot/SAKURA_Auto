744541c5fd935b4bd2c8065bc45069e9600fcd9b
Yyuiiu <13iori2014@gmail.com>
2025-08-14 17:31:23 +0800

    Update Excel loading from Secrets

--- SAKURA_App.py
+++ SAKURA_App.py
@@ -1,9 +1,23 @@
 import streamlit as st # type: ignore
 import pandas as pd
+import io
+import base64
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import smtplib
 from email.message import EmailMessage
+
+# SecretsからExcelを復元、読み込み
+excel_bytes = base64.b64decode(st.secrets["EXCEL_BASE64"])
+excel_io = io.BytesIO(excel_bytes)
+df = pd.read_excel(excel_io)
+
+# メンバー辞書
+allmenber = dict(zip(df["Name"], df["Player_No"]))
+staff_list = {name for name, num in allmenber.items() if isinstance(num, str) and not pd.isna(num)}
+player_list = {name: num for name, num in allmenber.items() if isinstance(num, (int,float)) and not pd.isna(num)}
+player_multiselect_list = [f"No,{num} {name}" for name, num in player_list.items()]
+
 
 #メール送信関数
 def send_email_with_image(image_path):
@@ -26,18 +40,9 @@ def send_email_with_image(image_path):
         server.send_message(msg)
     
 
-#本体
+#本体(Streamlit画面)
 st.title("hello SAKURA")
 st.write("made by me")
-
-#データ（メンバー辞書）
-df = pd.read_excel("SAKURA_list.xlsx")
-
-allmenber=dict(zip(df["Name"],df["Player_No"]))
-allmenber_list=[f"No,{s} {t}" for t ,s in allmenber.items()]
-staff_list={name for name, num in allmenber.items() if isinstance(num, str) and not pd.isna(num)}
-player_list={name: num for name, num in allmenber.items() if isinstance(num, (int,float)) and not pd.isna(num)}
-player_multiselect_list=[f"No,{num} {name}" for name ,num in player_list.items()]
 
 # セッション状態の初期化
 if "selected" not in st.session_state:
@@ -60,7 +65,6 @@ if st.session_state.get("confirmed", False):
     selected_menber=dict(sorted(selected_menber.items(), key=lambda item:item[1]))
 
     st.subheader(f"Staff {staff_list}")
-    
     st.subheader("Selected 11")
     for name,number in selected_menber.items():
         st.write(f"- {name} (No,{number})")
@@ -71,56 +75,38 @@ if st.session_state.get("confirmed", False):
     if st.button("OK"):
         
         try:
-            #現在日付
-            today=datetime.now().strftime("%Y/%m/%d")
-            #Excelファイルを読み込み
-            df=pd.read_excel("SAKURA_list.xlsx")
-            #新しい列を初期化（すべて空白）
-            df[today]=" "
-            #選ばれた人だけ "Selected" 
-            for name in selected_menber.keys():
-                df.loc[df["Name"]==name,today]="Serected"
-            for name in staff_list:
-                df.loc[df["Name"]==name,today]="Staff"
-            #保存
-            df.to_excel("SAKURA_list.xlsx",index=False)
-            st.success("Comp")
-
             #Pillowで画像生成
             template_path="Starting_XI.png" # Canvaで作った背景画像
-            output_path=f"MATCHDAY_{datetime.now().strftime('%Y%m%d')}.png"
-            template=Image.open(template_path).convert("RGBA")
+            output_path = f"MATCHDAY_{datetime.now().strftime('%Y%m%d')}.png"
+            template = Image.open(template_path).convert("RGBA")
             draw = ImageDraw.Draw(template)
-
             # フォント設定
             font_path = "NotoSansTC-SemiBold.ttf"
-            font=ImageFont.truetype(font_path,50)
+            font = ImageFont.truetype(font_path, 50)
 
-            # 番号を1列に描画
+            # 番号描画
+            y_start = 408
+            x_number = 253
+            line_height = 67
+            for idx, (name, number) in enumerate(selected_menber.items()):
+                y_pos = y_start + idx*line_height
+                draw.text((x_number, y_pos), f" {number}", font=font, fill="#f0edf0")
+
+            # 名前描画
             y_start=408
-            x_number=253
-            line_height=67 #1人分の行間
-            for idx,(name,number) in enumerate(selected_menber.items()):
-                y_pos=y_start+idx*line_height
-                draw.text((x_number,y_pos),f" {number}",font=font,fill="#f0edf0")
-
-            # 名前を1列に描画
-            y_start=408
-            x_name=337
-            line_height=67 #1人分の行間
-            for idx,(name,number) in enumerate(selected_menber.items()):
-                y_pos=y_start+idx*line_height
-                draw.text((x_name,y_pos),f" {name}",font=font,fill="#f0edf0")
-
-
+            x_name = 337
+            line_height=67
+            for idx, (name, number) in enumerate(selected_menber.items()):
+                y_pos = y_start + idx*line_height
+                draw.text((x_name, y_pos), f" {name}", font=font, fill="#f0edf0")
+            
             template.save(output_path)
-            st.image(output_path,caption="Starting11IMG",use_column_width=True)
-
-            st.success(f"Complete 画像生成完了:{output_path}")
+            st.image(output_path, caption="Starting11IMG", use_column_width=True)
+            st.success(f"Complete 画像生成完了: {output_path}")
 
             #メール送信を追加
             send_email_with_image(output_path)
-            st.success("Sended")
+            st.success("メール送信完了")
 
         except Exception as e:
             st.error(f"Error:{e}")
